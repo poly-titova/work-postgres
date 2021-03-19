5b50654fdbee18766346f5ce6410745ac1652c2b
Poly Titova <poly_titova@mail.ru>
2021-03-19 21:42:59 +0300

    Удалили таблицы

---
+++ schema.sql
@@ -0,0 +1,10 @@
+-- запросы для удаления таблиц
+DROP TABLE IF EXISTS books_readers;
+DROP TABLE IF EXISTS books_publishers;
+DROP TABLE IF EXISTS books_genres;
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS readers;
+DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS publishers;
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS countries;
