5096febe2332503eb9f1085bf4f197a87b06a35a
Poly Titova <poly_titova@mail.ru>
2021-03-21 14:48:59 +0300

    Сделали подсчёт одноимённых авторов

--- queries.sql
+++ queries.sql
@@ -1,3 +1,11 @@
 -- учитываются все строки результата для подсчёта количества
 SELECT COUNT(*)
 FROM books;
+
+-- считаются авторы с одной и той же фамилией
+SELECT
+  authors.lastname,
+  count(authors.lastname)
+FROM authors
+GROUP BY authors.lastname
+ORDER BY count(authors.lastname) DESC;
